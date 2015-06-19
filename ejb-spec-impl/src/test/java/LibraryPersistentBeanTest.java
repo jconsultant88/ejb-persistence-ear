7f511e649a1ccde0c2f99633da6d385a8817dc8f
jconsultant88 <jconsultant88@gmail.com>
2015-06-19 17:18:11 +0100

    Added a junit tests for EJB's
    Added junits and test-resources needed

---
+++ ejb-spec-impl/src/test/java/LibraryPersistentBeanTest.java
@@ -0,0 +1,57 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ejb.embeddable.EJBContainer;
+import javax.naming.Context;
+import javax.naming.NamingException;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.practise.ejb.service.LibraryPersistentBeanRemote;
+
+public class LibraryPersistentBeanTest {
+
+	private static Context ctx;
+	private static EJBContainer ejbContainer;
+
+	@BeforeClass
+	public static void setUp() throws IOException {
+
+		// FileUtils.copyFile(new
+		// File("target/test-classes/META-INF/persistence.xml"), new
+		// File("target/classes/META-INF/persistence.xml"));
+
+		System.out.println("Opening the container "
+				+ System.getProperty("user.dir"));
+		File[] modules = { new File("target/test-classes"),
+				new File("target/classes")
+
+		};
+		Map<String, Object> props = new HashMap<String, Object>();
+		//props.put(EJBContainer.MODULES, modules);
+		props.put("org.glassfish.ejb.embedded.glassfish.installation.root",
+				"src/test/resources/gfv3");
+
+		ejbContainer = EJBContainer.createEJBContainer(props);
+		ctx = ejbContainer.getContext();
+	}
+
+	@AfterClass
+	public static void tearDown() {
+		ejbContainer.close();
+		System.out.println("Closing the container");
+	}
+
+	@Test
+	public void testApp() throws NamingException {
+
+		LibraryPersistentBeanRemote persistentBean = (LibraryPersistentBeanRemote) ctx
+				.lookup("java:global/classes/LibraryPersistentBean");
+
+		persistentBean.addBook("EJB-Junits");
+
+	}
+}
